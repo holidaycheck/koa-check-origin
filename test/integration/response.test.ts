2de83c802eda47cfe4ff46d40f5f9c7a18f26e0c
Christian Rackerseder <git@echooff.de>
2018-10-24 14:03:50 +0200

    ⏱ Add timeout to integration tests

--- test/integration/response.test.ts
+++ test/integration/response.test.ts
@@ -4,6 +4,7 @@ import { withServer } from './test-server';
 
 test('server responds with HTTP 404 when origin matches', t =>
   withServer(async url => {
+    t.timeoutAfter(500);
     t.plan(1);
     try {
       await got(url, { method: 'GET' });
@@ -14,6 +15,7 @@ test('server responds with HTTP 404 when origin matches', t =>
 
 test('server responds with HTTP 403 when origin does not matches', t =>
   withServer(async url => {
+    t.timeoutAfter(500);
     t.plan(1);
     try {
       await got(url, { method: 'GET' });
