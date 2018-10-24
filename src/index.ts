8069bd6eec57a1f7f278ce1f20546d754d51a16b
Christian Rackerseder <git@echooff.de>
2018-10-24 15:24:43 +0200

    📖 Add JSDoc comment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { Context, Middleware } from 'koa';
 import { URL } from 'url';
 
+/**
+ * Creates a new koa middleware that checks if the given URL matches the origin.
+ * @param baseUrl the URL that should be checked on every request.
+ */
 export function createCheckOriginMiddleware(baseUrl: string): Middleware {
   const { hostname: allowedHost } = new URL(baseUrl);
+
   return async (ctx: Context, next: () => Promise<any>) => {
     const { origin } = ctx;
     const { hostname: originHost } = new URL(origin);
